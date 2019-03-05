67d7f6ca81bc18c48044f63efeebe08d70cbb2e1
unknown <ochafloquet@gmail.com>
2019-03-05 01:11:53 -0500

    proyecto final de JQUERY

--- AppBrujulaJQuery/Controller.js
+++ AppBrujulaJQuery/Controller.js
@@ -1,21 +1,33 @@
+var transaccion = [];
 $(document).ready(function() {
-
+    $( "#result" ).hide();
     $.getJSON( "/paises", { paises: "SudAmerica" },function( data ) {
         var options_html = ['<option value="">Seleccione</option>'];
         $.each(data, function (index, pais) {
-            options_html.push("<option value='" + pais.id + "'>" + pais.name + "</option>");
+            options_html.push("<option value='" + pais.code + "'>" + pais.name + "</option>");
         });
         $('select#paises').html(options_html.join('')).prop('disabled', false);
 
     });
 
     $("#btn_remove").click(function() {
-         var optionText = $("#paises option:selected").text();
-        if(optionText!=""){
-            if (confirm('Estas Seguro de Eliminar...!!')) {
-                $("#paises option:selected").remove();
-            }
+        var code  = $("#paises option:selected").val();
+         var name = $("#paises option:selected").text();
+         console.log(code+'-'+name);
+        if(name!=""){
+            $("#mi-modal").modal('show');
 
+            $("#modal-btn-si").on("click", function(){
+                $("#paises option:selected").remove();
+                transaccion = [];
+                transaccion.push ('remove',code,name);
+                $("#mi-modal").modal('hide');
+            });
+
+            $("#modal-btn-no").on("click", function(){
+
+                $("#mi-modal").modal('hide');
+            });
         }
     });
 
@@ -25,13 +37,53 @@ $(document).ready(function() {
         var name = $('#txt_name').val();
 
         if(txt_code!=""&&txt_name!=""){
-            $("#paises").append("<option value='item" + code + "'>" + name + "</option>");
+            $("#paises").append("<option value='" + code + "'>" + name + "</option>");
             $('#myModal').modal('toggle');
+            transaccion = [];
+            transaccion.push ('add', code, name);
+
         }else{
             alert("Ingresar datos solicitados");
             return;
         }
     });
+
+
+
+    $("#btn_back").click(function() {
+
+        for(var i=0; i<transaccion.length; i++){
+
+            if(transaccion[i] == "add"){
+                console.log("value1"+transaccion[1]);
+                var code= transaccion[1];
+                $("#paises option[value= '" + code + "']").remove();
+                $( "#result" ).show();
+                $("#result").html("Se elimino el pais agregado");
+                setTimeout(function() {
+                    $( "#result" ).hide();
+                }, 2000);
+            }
+            if(transaccion[i] == "remove"){
+                var code= transaccion[1];
+                var name= transaccion[2];
+                $("#paises").append("<option value='" + code + "'>" + name + "</option>");
+                $( "#result" ).show();
+                $("#result").html("Se agrego el pais eliminado");
+                setTimeout(function() {
+                    $( "#result" ).hide();
+                }, 2000);
+            }
+
+
+
+        }
+
+        //
+
+
+    });
+
 
 
 
